c827c400d9dc2d257e19a42433385086ad29f2e2
Profound Games <i.wijma@ziggo.nl>
2015-06-17 17:55:38 +0200

    Initial Commit
    Added ckeditor, materialize and jquery.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="/css/default.css">
+        <link rel="stylesheet" href="/css/materialize.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        
+        <script src="/js/jquery-2.1.4.min.js"></script>
+        <script src="/js/materialize.min.js"></script>
+        
+    </head>
+    <body class="">
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
